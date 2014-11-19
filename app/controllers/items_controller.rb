d6f178df881bdaddbc9a771578cf87ab799a51fd
Richard <rich.miller2012@gmail.com>
2014-11-19 01:55:44 +0000

    test

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-
+  #Rich added this comments here
   # GET /items
   # GET /items.json
   def index
