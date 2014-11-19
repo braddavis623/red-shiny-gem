4af31e7552c8140efed63f8dba86e0e2c0910175
John Harrison <Cratezthebox@gmail.com>
2014-11-19 01:40:42 +0000

    Added Seperate Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'greetings/hello'
+  root 'greetings#hello'
   resources :items
-
   resources :categories
 
   # The priority is based upon order of creation: first created -> highest priority.
